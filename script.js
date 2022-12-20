2cb8b79315bba04298c3af06ccae689dd995f61e
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-20 15:27:05 +0700

    style(*): format DOM

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector('.btn-open')
 const form = document.querySelector('.fact-form')
+const factsList = document.querySelector('.facts-list')
+
+factsList.innerHTML = ''
 
 btn.addEventListener('click', function () {
   if (form.classList.contains('hidden')) {
