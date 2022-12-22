7986756072ef619847916f935e2c07652731af79
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-22 19:49:24 +0700

    chore(dev): clean up the src and add overflow style

--- src/App.js
+++ src/App.js
@@ -14,39 +14,6 @@ const CATEGORIES = [
   { name: 'news', color: '#8b5cf6' }
 ]
 
-const initialFacts = [
-  {
-    id: 1,
-    text: 'React is being developed by Meta (formerly facebook)',
-    source: 'https://opensource.fb.com/',
-    category: 'technology',
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021
-  },
-  {
-    id: 2,
-    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
-    source: 'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-    category: 'society',
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019
-  },
-  {
-    id: 3,
-    text: 'Lisbon is the capital of Portugal',
-    source: 'https://en.wikipedia.org/wiki/Lisbon',
-    category: 'society',
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015
-  }
-]
-
 function App() {
   const [showForm, setShowForm] = useState(false)
   const [facts, setFacts] = useState([])
@@ -261,7 +228,6 @@ function Fact({ fact, setFacts }) {
       .eq('id', fact.id)
       .select()
     setIsUpdating(false)
-    console.log(updatedFact)
 
     if (!error) setFacts(facts => facts.map(f => (f.id === fact.id ? updatedFact[0] : f)))
   }
